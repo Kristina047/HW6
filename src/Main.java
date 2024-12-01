59ee328527048d2887872938124bfa8c965745c7
Kristina047 <ch.drobot@gmail.com>
2024-12-01 18:09:31 +0300

    дз циклы 2

--- src/Main.java
+++ src/Main.java
@@ -33,13 +33,69 @@ public class Main {
             System.out.println("Год " + i + " ,численность населения составляет " + population);
         }
         System.out.println("Задача 4");
-        int depositOne = 15000;
-        float percent = 0.07f;
-        int total = 0;
-        for (int i = 1; i <= 79; i++) {
-            {
-                System.out.println("Месяц " + i + " ,сумма составляет " + total1);
+
+        int firstSum = 15000;
+        int lastSum = 12_000_000;
+        int countMonth = 0;
+
+        while (firstSum <= lastSum) {
+            firstSum = (int) (firstSum + firstSum * 0.07);
+            countMonth++;
+            System.out.println("За " + countMonth + " месяц сумма накоплений состаляет " + firstSum);
+
+        }
+        System.out.println("Задача 5");
+
+        int firstSum1 = 15000;
+        int lastSum1 = 12_000_000;
+        int countMonth1 = 5;
+
+        while (firstSum1 <= lastSum1) {
+            firstSum1 = (int) (firstSum1 + firstSum1 * 0.07);
+            countMonth1++;
+            if (countMonth1 % 6 == 0) {
+                System.out.println("За " + countMonth1 + " месяц сумма накоплений состаляет " + firstSum1);
+
+            }
+            System.out.println("Задача 6");
+
+            int firstSum2 = 15000;
+            int lastSum2 = 12_000_000;
+            int countMonth2 = 0;
+            int months = 12 * 9;
+
+            while (countMonth2 <= months) {
+                firstSum2 = (int) (firstSum2 + firstSum2 * 0.07);
+                countMonth2++;
+                if (countMonth2 % 6 == 0) {
+                    System.out.println("За " + countMonth2 + " месяц сумма накоплений состаляет " + firstSum2);
+                }
+            }
+
+            System.out.println("Задача 7");
+            int firstFriday = 6;
+            for (int day = firstFriday; day <= 31; day += 7) {
+                System.out.println("Сегодня пятница " + day + "ое число. Необходимо подготовить отчет.");
+            }
+
+            System.out.println("Задача 8");
+            int spanKamets = 79;
+            int startDate = 200;
+            int finalDate = 100;
+            int currentYear = 2023;
+            int pastBecentenary = currentYear - startDate;
+            for (int year1 = 0; year1 <= currentYear + finalDate; year1 = year1 + spanKamets) {
+                if (year1 >= pastBecentenary) {
+                    System.out.println(year1);
+                    if (year1 > currentYear) {
+                        break;
+                    }
+                }
             }
         }
     }
 }
+
+
+
+
